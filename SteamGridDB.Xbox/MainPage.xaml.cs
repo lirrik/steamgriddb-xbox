46e4330d3efd9dda08ade0cea216a6741d808e94
Kyrylo Derevianko <kir.basin@gmail.com>
2025-11-09 01:42:06 +1100

    Show instructions when the app is launched directly from Windows instead of Xbox Game Bar

--- SteamGridDB.Xbox/MainPage.xaml.cs
+++ SteamGridDB.Xbox/MainPage.xaml.cs
@@ -3,7 +3,7 @@
 namespace SteamGridDB.Xbox
 {
     /// <summary>
-    /// An empty page that opens if the widget app is treated as a regular UWP app (for example, when debugging).
+    /// Page that opens if the widget app is launched as a regular UWP app instead of Xbox Game Bar widget (for example, from the Start menu or when debugging).
     /// </summary>
     public sealed partial class MainPage : Page
     {
